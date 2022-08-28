b81012856e5209d72721627f9f472e28a053b74a
shashank-gv <shashank.gummuluru@gmail.com>
2022-08-28 14:17:31 +1000

    refactor code to comply with flake8

--- sentiment.py
+++ sentiment.py
@@ -10,15 +10,17 @@ def sentiment_pipeline(text, classifier):
     sentiments = classifier(text)
     return sentiments
 
+
 def print_sents(text, sentiments):
     if(type(text) == str):
         list_text = []
         list_text.append(text)
         text = list_text.copy()
-    for i, j in zip(text,sentiments):
+    for i, j in zip(text, sentiments):
         print(f"{i} , {j['label']}, {j['score']}")
 
-parser = argparse.ArgumentParser(description ='provide the sentiment ((pos | neg), score) of a given piece of text')
+
+parser = argparse.ArgumentParser(description=utils.descriptions["sentiment"])
 parser.add_argument('text', type=str, default=sys.stdin)
 args = parser.parse_args()
 # print(args.text)
@@ -26,10 +28,12 @@ args = parser.parse_args()
 try:
     text = utils.read_file_lines(args.text)
     # print(text)
-except:
+except FileNotFoundError:
     text = args.text
     # print(text)
 
-sent_classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+SENT_MODEL = "distilbert-base-uncased-finetuned-sst-2-english"
+
+sent_classifier = pipeline("sentiment-analysis", model=SENT_MODEL)
 sentiments = sentiment_pipeline(text, sent_classifier)
 print_sents(text, sentiments)
--- utils.py
+++ utils.py
@@ -5,7 +5,8 @@ import re
 
 def check_url(url):
     url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
-    return re.match(url_pattern, url) 
+    return re.match(url_pattern, url)
+
 
 def read_file(file_name):
     path = f"{get_cmd_cwd()+'/'+file_name}"
@@ -13,13 +14,20 @@ def read_file(file_name):
     text = file.read()
     return text
 
+
 def read_url(url):
     downloaded = trafilatura.fetch_url(url)
     pretty_str = trafilatura.extract(downloaded)
     return pretty_str
 
+
 def read_file_lines(file_name):
     path = f"{file_name}"
     with open(path) as f:
-        lines = [line.rstrip() for line in f] # ignore \n
+        lines = [line.rstrip() for line in f]  # ignore \n
     return lines
+
+
+descriptions = {"summarize": 'summarize a given piece of text',
+                "sentiment": 'provide the sentiment ((pos | neg), score) \
+                     of a given piece of text'}
