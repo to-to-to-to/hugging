ad1ed30c473b9eb62c5746fd2b2db76dcc9f2f37
shashank-gv <shashank.gummuluru@gmail.com>
2022-09-20 22:40:32 +1000

    Added better command line demo for sentiment

--- demo.py
+++ demo.py
@@ -11,7 +11,14 @@ if(args.command not in utils.commands):
     exit(1)
 
 if(args.command == "sentiment"):
-    os.system("python3 sentiment.py sent_text.txt")
+    import sentiment
+    introduction = """Sentiment analysis is the process of determining whether a piece of text is positive, negative, or neutral. This can be done using a variety of methods, but one popular approach is to use a transformer model. We use the HuggingFace library to help us build our models. This demo function will show you the input/output of a simple transformer model that predicts the sentiment of a statement. """
+    print(introduction)
+    print("Enter a statement to be analyzed:", end=' ')
+    statement = input()
+    print("Analyzing...")
+    sentiment.build_sent(statement)
 
 if(args.command == "summarize"):
+    import summarize
     os.system("python3 summarize.py microsoft_text.txt --min_len 50 --max_len 100 --demo True") # noqa : E501
