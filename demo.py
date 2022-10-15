4458f3827aff5ec8e6b58164fdddf305f92cdf47
shashank-gv <shashank.gummuluru@gmail.com>
2022-10-16 01:28:20 +1100

    added basic demo command for summarize

--- demo.py
+++ demo.py
@@ -35,8 +35,23 @@ if(args.command == "sentiment"):
         sentiments.append((sentiment.build_sent(i)))
     mlcat(title="Corresponding Sentiments", text="", end="")
     for i in sentiments:
-        print(sentiment.output_sent(i))
+        print("{:.2f}".format(sentiment.output_sent(i)))
 
 
 if(args.command == "summarize"):
+    introduction = """Machine learning text summarization works by taking a \
+        large amount of text and reducing it to its most important points. \
+        This is done by using algorithms that identify the main ideas in the \
+        text and then create a summary based on those ideas. \
+        This is achieved by transformers by training a model on a large\
+        amount of data and then fine-tuning it to be able to summarise text. \
+        We use the hugging face repository to leverage these transfomers. \
+        This demo function will walk you through a simple example and \
+        translate microsoft_text.txt which is present in the /.mluhub \
+        directory for reference. """
+    mlcat(title="Summarization pipeline showcase", text=introduction) # noqa : E501
+    mlask(begin="", end="\n",
+          prompt="Press Enter to display the summary of microsoft_text.txt")
+    print("Analysing...")
+    mlcat(title="Summary of microsoft_text.txt", text="", end="")
     os.system("python3 summarize.py microsoft_text.txt --min_len 50 --max_len 100 --demo True") # noqa : E501
